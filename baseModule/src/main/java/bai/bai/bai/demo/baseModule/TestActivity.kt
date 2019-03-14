d46efe975f4e1d58311873cf1a9b62cccbf3ad68
baiyunyan <baiyunyan@wiseasy.com>
2019-03-14 11:57:39 +0800

    添加baseModule

---
+++ baseModule/src/main/java/bai/bai/bai/demo/baseModule/TestActivity.kt
@@ -0,0 +1,13 @@
+package bai.bai.bai.demo.baseModule
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class TestActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test)
+
+    }
+}
