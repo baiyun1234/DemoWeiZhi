4c0459a9fc304007e2a9d97148e55f5457d31d94
baiyunyan <baiyunyan@wiseasy.com>
2019-11-19 11:47:15 +0800

    ActionBar设置成悬浮式

--- app/src/main/java/bai/bai/bai/demo/activity/ActionBarActivity.kt
+++ app/src/main/java/bai/bai/bai/demo/activity/ActionBarActivity.kt
@@ -2,12 +2,28 @@ package bai.bai.bai.demo.activity
 
 import android.app.Activity
 import android.os.Bundle
+import android.support.v4.view.WindowCompat
+import android.view.Window
 import bai.bai.bai.demo.R
+import bai.bai.bai.demo.view.BaseActionBar
+import kotlinx.android.synthetic.main.activity_action_bar.*
 
 class ActionBarActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //复制粘贴布局设置成悬浮式
+        requestWindowFeature(Window.FEATURE_ACTION_MODE_OVERLAY);//如果是继承Activity类则用requestWindowFeature方法
+//        supportRequestWindowFeature(WindowCompat.FEATURE_ACTION_MODE_OVERLAY)//如果是继承ActionBar或AppCompatActivity则使用supportRequestWindowFeature
         setContentView(R.layout.activity_action_bar)
+
+        action_bar.setActionBarClickListener(object : BaseActionBar.ActionBarClickListener {
+            override fun onActionBarRight() {
+            }
+
+            override fun onActionBarLeft() {
+                finish()
+            }
+        })
     }
 }
